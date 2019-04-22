91534024551adb337822549f3fa82bb5d68b3389
BorkoG <borko_3g@mail.com>
2019-04-22 21:33:34 +0100

    Added sensor reading and transmitting over html on HTTP request.

--- ESP8266-Binmeter/ESP8266-Binmeter.ino
+++ ESP8266-Binmeter/ESP8266-Binmeter.ino
@@ -1,5 +1,5 @@
 /*
- *  ESP8266 Sketch that reads the distance from one ultrasound snesor and 
+ *  ESP8266 Sketch that reads the distance from one ultrasound sensor and
  *  sends it over HTTP on demand.
  * 
  *  Authors: Borislav Gachev, Ruigang Chen, Ronaldas Gadzimugometovas
@@ -24,10 +24,39 @@
 ESP8266WebServer server(80);
 
 //TODO: Include pseudo-code that would read from the HC-SR04 sensor based on schematics and earlier sketch
+#define ECHO_PIN 15
+#define TRIG_PIN 12
+#define PULSE_WAIT_TIME 15000 //in microseconds
+#define MAX_PULSE_DURATION 2942 // multiply by 0.017 to get max distance [50cm atm]
+
+long  duration;
+int   distance;
+
+
+void pollSensor(){
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(12);
+  digitalWrite(TRIG_PIN, LOW);
+
+  duration = pulseIn(ECHO_PIN, HIGH, PULSE_WAIT_TIME);
+
+  if(duration > 0){
+    distance = duration * 0.017;
+  }
+
+}
 
 
 void handleRequest(){
-  String html = "<h3>Welcome to our simple ESP8266 page :)</h3>";
+
+  pollSensor();
+
+  String html ="<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"><title>Smart bin-meter</title><meta name=\"description\" content=\"Bin-meter readings\"><meta name=\"author\" content=\"DST 2019 Home automation team\"></head><body><h2 style=\"color: #5e9ca0;display: flex; flex-direction: column; justify-content: center; text-align: center;\">Welcome to our Bin-meter page :)</h2><div style=\"color: #2e6c80; display: flex; flex-direction: column; justify-content: center; text-align: center;\" id=\"distanceDiv\">Current cm distance to rubbish:<div id=\"distanceVal\">";
+  html += distance;
+  html +="</div></div><div style=\"color: #2e6c80; display: flex; flex-direction: column; justify-content: center; text-align: center;\" id=\"durationDiv\">Current pulse duration in ms:<div id=\"durationVal\">";
+  html += duration;
+  html +="</div></div></body></html>";
+
   server.send(200, "text/html", html);
 }
 
@@ -47,6 +76,11 @@ void setup() {
   server.begin();
   
   Serial.println("Request handler started.");
+
+  pinMode(TRIG_PIN, OUTPUT);
+  digitalWrite(TRIG_PIN, LOW);
+  pinMode(ECHO_PIN, INPUT);
+
 }
 
 void loop() {
